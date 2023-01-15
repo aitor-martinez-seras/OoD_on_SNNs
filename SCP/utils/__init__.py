998650e8a40efe0d7611a782cb9c791e56bcb489
110414 <aitor.martinez@tecnalia.com>
2023-01-15 16:23:11 +0100

    folders

--- SCP/utils/__init__.py
+++ SCP/utils/__init__.py
@@ -1,6 +1,1 @@
-from utils import clusters
-from utils import common
-from utils import constants
-from utils import explainability
-from utils import metrics
-from utils import plots
+from SCP.utils import explainability
