ca23dea661e6115731443c506b031e6ea1d87a4f
110414 <aitor.martinez@tecnalia.com>
2023-01-16 12:07:30 +0100

    update paths

--- SCP/utils/datasets/mnist.py
+++ SCP/utils/datasets/mnist.py
@@ -1,4 +1,5 @@
 import random
+from pathlib import Path
 
 import torch
 import torchvision
@@ -6,29 +7,25 @@ import torchvision
 from constants import DATASETS_PATH
 
 
-def load_MNIST(batch_size, test_only=False):
+def load_MNIST(batch_size, datasets_path: Path, test_only=False):
     transform = torchvision.transforms.Compose(
         [
             torchvision.transforms.ToTensor(),
         ]
     )
     test_data_MNIST = torchvision.datasets.MNIST(
-        root=DATASETS_PATH,
+        root=datasets_path,
         train=False,
         download=True,
         transform=transform,
     )
     test_loader_MNIST = torch.utils.data.DataLoader(
-        torchvision.datasets.MNIST(
-            root=DATASETS_PATH,
-            train=False,
-            transform=transform,
-        ),
+        test_data_MNIST,
         batch_size=batch_size
     )
     if test_only is False:
         train_data_MNIST = torchvision.datasets.MNIST(
-            root=DATASETS_PATH,
+            root=datasets_path,
             train=True,
             download=True,
             transform=transform,
@@ -56,7 +53,7 @@ def square_creation(input_tensor: torch.Tensor):
     return input_tensor
 
 
-def load_MNIST_square(batch_size, test_only=True, *args):
+def load_MNIST_square(batch_size, datasets_path: Path, *args):
     transform = torchvision.transforms.Compose(
         [
             torchvision.transforms.ToTensor(),
@@ -64,17 +61,13 @@ def load_MNIST_square(batch_size, test_only=True, *args):
         ]
     )
     test_data_MNIST_square = torchvision.datasets.MNIST(
-        root=DATASETS_PATH,
+        root=datasets_path,
         train=False,
         download=True,
         transform=transform,
     )
     test_loader_MNIST_square = torch.utils.data.DataLoader(
-        torchvision.datasets.MNIST(
-            root=DATASETS_PATH,
-            train=False,
-            transform=transform,
-        ),
+        test_data_MNIST_square,
         batch_size=batch_size
     )
     return test_loader_MNIST_square
