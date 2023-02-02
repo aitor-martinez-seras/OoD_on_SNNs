6cbcf787506628df2f02d93ce36fec96c0d4f720
110414 <aitor.martinez@tecnalia.com>
2023-02-02 14:09:46 +0100

    add RandomCrop for one model training

--- SCP/datasets/cifar.py
+++ SCP/datasets/cifar.py
@@ -50,7 +50,7 @@ def load_CIFAR10(batch_size, datasets_path: Path, test_only=False, image_shape=(
                 # T.RandomRotation(15, ),
                 # T.RandomCrop(400),
                 T.ToTensor(),
-                # T.RandomCrop(32, padding=4),
+                T.RandomCrop(32, padding=4),
                 T.RandomHorizontalFlip(),
                 # T.RandomVerticalFlip(),
 
