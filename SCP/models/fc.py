a7cc081f845c9a2c8fb4d8d7a372d47d88666233
110414 <aitor.martinez@tecnalia.com>
2023-02-07 09:27:36 +0100

    new model for OODGenomics

--- SCP/models/fc.py
+++ SCP/models/fc.py
@@ -457,3 +457,95 @@ class FCSNN6(torch.nn.Module):
             raise NameError('Wrong flag')
 
         return vo, hdn_spikes
+
+
+class FCSNN7(torch.nn.Module):
+    """
+    Bigger model, 4 linear layers (8192-2048-512-128, dividing by 4 all in each layer)
+    """
+    def __init__(self, input_features, hidden_features, output_features, dt=0.001):
+        super().__init__()
+
+        self.thr = 0.5
+
+        # Linear layers
+        self.fc1 = torch.nn.Linear(input_features, 8192, bias=False)
+        self.fc2 = torch.nn.Linear(8192, 2048, bias=False)
+        self.fc3 = torch.nn.Linear(2048, 512, bias=False)
+        self.fc4 = torch.nn.Linear(512, hidden_features, bias=False)  # The idea is to use 128
+        self.fc_out = torch.nn.Linear(hidden_features, output_features, bias=False)
+
+        # Neurons
+        self.lif1 = LIFCell(p=LIFParameters(alpha=100, v_th=torch.tensor(self.thr)), dt=dt)
+        self.lif2 = LIFCell(p=LIFParameters(alpha=100, v_th=torch.tensor(self.thr)), dt=dt)
+        self.lif3 = LIFCell(p=LIFParameters(alpha=100, v_th=torch.tensor(self.thr)), dt=dt)
+        self.lif4 = LIFCell(p=LIFParameters(alpha=100, v_th=torch.tensor(self.thr)), dt=dt)
+        self.out = LICell(dt=dt)
+
+        self.input_features = input_features
+        self.hidden_features = hidden_features
+        self.output_neurons = output_features
+
+    def forward(self, x, flag=None):
+        # Input shape = [time_step,batch_size, ... , ... , ... ]
+        seq_length, batch_size, _ = x.shape
+        s1 = s2 = s3 = s4 = so = None
+        if flag is None:
+            for ts in range(seq_length):
+                # Flatten the input to [batch_size, input_features]
+                z = x[ts]
+
+                # Hidden layer 1
+                z = self.fc1(z)
+                z, s1 = self.lif1(z, s1)
+
+                # Hidden layer 2
+                z = self.fc2(z)
+                z, s2 = self.lif2(z, s2)
+
+                # Hidden layer 3
+                z = self.fc3(z)
+                z, s3 = self.lif3(z, s3)
+
+                # Hidden layer 4
+                z = self.fc4(z)
+                z, s4 = self.lif4(z, s4)
+
+                # Output layer
+                z = self.fc_out(z)
+                vo, so = self.out(z, so)
+            return vo
+
+        elif flag == "hidden_spikes_and_logits":
+            hdn_spikes = torch.zeros(
+                seq_length, batch_size, self.hidden_features,
+                device=x.device, dtype=x.dtype
+            )
+            for ts in range(seq_length):
+                # Flatten the input to [batch_size, input_features]
+                z = x[ts, :, :, :].view(-1, self.input_features)
+
+                # Hidden layer 1
+                z = self.fc1(z)
+                z, s1 = self.lif1(z, s1)
+
+                # Hidden layer 2
+                z = self.fc2(z)
+                z, s2 = self.lif2(z, s2)
+
+                # Hidden layer 3
+                z = self.fc3(z)
+                z, s3 = self.lif3(z, s3)
+
+                # Hidden layer 4
+                z = self.fc4(z)
+                z, s4 = self.lif4(z, s4)
+                hdn_spikes[ts] = z
+
+                # Output layer
+                z = self.fc_out(z)
+                vo, so = self.out(z, so)
+        else:
+            raise NameError('Wrong flag')
+
+        return vo, hdn_spikes
