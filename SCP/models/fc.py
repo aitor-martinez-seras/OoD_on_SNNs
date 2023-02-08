b7f325178af0741fdd86c7f27ee051ee9be81851
110414 <aitor.martinez@tecnalia.com>
2023-02-08 18:12:42 +0100

    new model fc9

--- SCP/models/fc.py
+++ SCP/models/fc.py
@@ -572,3 +572,84 @@ class FC8(torch.nn.Module):
         z = self.fc_out(z)
 
         return z
+
+
+class FCSNN9(torch.nn.Module):
+    """
+    2 linear layers with 400-100
+    """
+    def __init__(self, input_features, hidden_features, output_features, dt=0.001):
+        super().__init__()
+
+        self.thr = 1
+
+        # Linear layers
+        self.fc1 = torch.nn.Linear(input_features, 400, bias=False)
+        self.fc2 = torch.nn.Linear(400, hidden_features, bias=False)  # The idea is to use 128
+        self.fc_out = torch.nn.Linear(hidden_features, output_features, bias=False)
+
+        # Neurons
+        self.lif1 = LIFCell(p=LIFParameters(alpha=100, v_th=torch.tensor(self.thr)), dt=dt)
+        self.lif2 = LIFCell(p=LIFParameters(alpha=100, v_th=torch.tensor(self.thr)), dt=dt)
+        self.lif3 = LIFCell(p=LIFParameters(alpha=100, v_th=torch.tensor(self.thr)), dt=dt)
+        self.out = LICell(dt=dt)
+
+        self.input_features = input_features
+        self.hidden_features = hidden_features
+        self.output_neurons = output_features
+
+    def forward(self, x, flag=None):
+        # Input shape = [time_step,batch_size, ... , ... , ... ]
+        seq_length, batch_size, _ = x.shape
+        s1 = s2 = s3 = so = None
+        if flag is None:
+            for ts in range(seq_length):
+                # Flatten the input to [batch_size, input_features]
+                z = x[ts]
+
+                # Hidden layer 1
+                z = self.fc1(z)
+                z, s1 = self.lif1(z, s1)
+
+                # Hidden layer 2
+                z = self.fc2(z)
+                z, s2 = self.lif2(z, s2)
+
+                # Hidden layer 3
+                z = self.fc3(z)
+                z, s3 = self.lif3(z, s3)
+
+                # Output layer
+                z = self.fc_out(z)
+                vo, so = self.out(z, so)
+            return vo
+
+        elif flag == "hidden_spikes_and_logits":
+            hdn_spikes = torch.zeros(
+                seq_length, batch_size, self.hidden_features,
+                device=x.device, dtype=x.dtype
+            )
+            for ts in range(seq_length):
+                # Flatten the input to [batch_size, input_features]
+                z = x[ts, :, :, :].view(-1, self.input_features)
+
+                # Hidden layer 1
+                z = self.fc1(z)
+                z, s1 = self.lif1(z, s1)
+
+                # Hidden layer 2
+                z = self.fc2(z)
+                z, s2 = self.lif2(z, s2)
+
+                # Hidden layer 3
+                z = self.fc3(z)
+                z, s3 = self.lif3(z, s3)
+                hdn_spikes[ts] = z
+
+                # Output layer
+                z = self.fc_out(z)
+                vo, so = self.out(z, so)
+        else:
+            raise NameError('Wrong flag')
+
+        return vo, hdn_spikes
--- SCP/models/model.py
+++ SCP/models/model.py
@@ -7,7 +7,7 @@ from norse.torch import PoissonEncoder
 from norse.torch import ConstantCurrentLIFEncoder
 from norse.torch import LIFParameters
 
-from SCP.models.fc import FCSNN1, FCSNN2, FCSNN3, FCSNN4, FCSNN5, FCSNN6, FCSNN7, FC8
+from SCP.models.fc import FCSNN1, FCSNN2, FCSNN3, FCSNN4, FCSNN5, FCSNN6, FCSNN7, FC8, FCSNN9
 from SCP.models.conv import ConvSNN1, ConvSNN2, ConvSNN3, ConvSNN5, ConvSNN4, ConvSNN6, LIFConvNet, ConvSNN9, \
     ConvSNN10, ConvSNN11_no_dropout, ConvSNN8, ConvSNN12, ConvSNN13, ConvSNN14, ConvSNN16, ConvSNN15, ConvSNN17, \
     ConvSNN18, ConvSNN19_max_pooling, ConvSNN20
@@ -210,6 +210,16 @@ def load_model(model_arch: str, input_size: list, hidden_neurons=None, output_ne
                 snn=FC8(input_features=input_size,
                         hidden_features=hidden_neurons,
                         output_features=output_neurons),
+                decoder=decode
+            )
+
+        elif n_hidden_layers == 9:  # Model for OODGenomics
+            assert n_time_steps == 250, 'Number of timesteps must be 250 for OODGenomics'
+            model = Model(
+                encoder=no_encoder,
+                snn=FCSNN9(input_features=input_size,
+                           hidden_features=hidden_neurons,
+                           output_features=output_neurons),
                 decoder=decode
             )
 
