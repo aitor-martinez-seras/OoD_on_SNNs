b6614185e29ccf31c8d108ca6a45ae7509ce6d13
110414 <aitor.martinez@tecnalia.com>
2023-02-13 10:45:00 +0100

    fix script

--- program_benchmark.sh
+++ program_benchmark.sh
@@ -1,5 +1,5 @@
 #!/usr/bin bash
 
-CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --f-max 200 --n-time-steps 64 --cluster-mode predictions --use-only-correct-test-images
-CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --f-max 200 --n-time-steps 64 --cluster-mode correct-predictions --use-only-correct-test-images
-CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --f-max 200 --n-time-steps 64 --cluster-mode labels --use-only-correct-test-images
+CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --pretrained --f-max 100 --n-time-steps 50 --cluster-mode predictions --use-only-correct-test-images
+CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --pretrained --f-max 100 --n-time-steps 50 --cluster-mode correct-predictions --use-only-correct-test-images
+CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --pretrained --f-max 100 --n-time-steps 50 --cluster-mode labels --use-only-correct-test-images
