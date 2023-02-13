9e1052a051d6ab5837cc2473e6b0958f44bd45d2
110414 <aitor.martinez@tecnalia.com>
2023-02-13 10:42:47 +0100

    add script

--- program_benchmark.sh
+++ program_benchmark.sh
@@ -1,4 +1,5 @@
 #!/usr/bin bash
 
-CUDA_VISIBLE_DEVICES=1 python main.py --conf rgb-benchmark --n-hidden-layers 10 --f-max 200 --n-time-steps 64 --cluster-mode labels
-CUDA_VISIBLE_DEVICES=1 python main.py --conf rgb-benchmark --n-hidden-layers 10 --f-max 200 --n-time-steps 64 --cluster-mode predictions
+CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --f-max 200 --n-time-steps 64 --cluster-mode predictions --use-only-correct-test-images
+CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --f-max 200 --n-time-steps 64 --cluster-mode correct-predictions --use-only-correct-test-images
+CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --f-max 200 --n-time-steps 64 --cluster-mode labels --use-only-correct-test-images
