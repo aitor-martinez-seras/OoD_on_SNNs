b1d1f0d62c9dbb6d162d7997fe307e26e2133124
110414 <aitor.martinez@tecnalia.com>
2023-02-14 14:40:22 +0100

    fix omniglot

--- SCP/datasets/omniglot.py
+++ SCP/datasets/omniglot.py
@@ -5,6 +5,7 @@ import torchvision
 from torchvision.transforms import Lambda
 
 from SCP.datasets.utils import parse_size_of_dataloader
+from SCP.utils.plots import show_img_from_dataloader, show_grid_from_dataloader
 
 
 def load_omniglot(batch_size, datasets_path: Path, *args, **kwargs):
@@ -27,3 +28,12 @@ def load_omniglot(batch_size, datasets_path: Path, *args, **kwargs):
         shuffle=False
     )
     return test_loader
+
+
+if __name__ == "__main__":
+    test_loader = load_omniglot(
+        64, Path(r"C:/Users/110414/PycharmProjects/OoD_on_SNNs/datasets"),
+    )
+    print(len(test_loader.dataset))
+    show_img_from_dataloader(test_loader, img_pos=0, number_of_iterations=1)
+    show_grid_from_dataloader(test_loader)
