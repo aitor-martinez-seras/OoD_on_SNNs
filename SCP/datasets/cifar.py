bd9ceb1a8398b3b8980cfe18e11c96c55d9f84d4
110414 <aitor.martinez@tecnalia.com>
2023-02-27 15:04:55 +0100

    change train trf order

--- SCP/datasets/cifar.py
+++ SCP/datasets/cifar.py
@@ -32,11 +32,11 @@ class CIFAR10(DatasetCustomLoader):
     def _train_transformation(self, output_shape):
         return T.Compose(
             [
-                T.ToTensor(),
-                T.Resize(output_shape),
                 # T.RandomCrop(output_shape[0], padding=4),
                 # T.RandomRotation(15, ),
                 T.RandomHorizontalFlip(),
+                T.ToTensor(),
+                T.Resize(output_shape),
 
             ]
         )
