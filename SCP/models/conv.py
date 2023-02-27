1ec2921dc33a7d81ea26c484001aece74c3f61cb
110414 <aitor.martinez@tecnalia.com>
2023-02-27 09:58:26 +0100

    cosmetic changes

--- SCP/models/conv.py
+++ SCP/models/conv.py
@@ -1377,7 +1377,6 @@ class ConvSNN10(nn.Module):
 
 class ConvSNN11_no_dropout(nn.Module):
     def __init__(self, input_size, hidden_neurons, output_neurons, alpha=100):
-        # super(ConvNet, self).__init__()
         super().__init__()
 
         self.ftmaps_h = int(((input_size[1] / 2) / 2) - 2 - 2)
@@ -1392,7 +1391,7 @@ class ConvSNN11_no_dropout(nn.Module):
         self.avgpool = nn.AvgPool2d(kernel_size=2)
 
         # Linear part
-        self.fc1 = nn.Linear(4 * 4 * 128, hidden_neurons, bias=False)
+        self.fc1 = nn.Linear(4 * 4 * 128, hidden_neurons, bias=False)  # 2048
         self.fc_out = nn.Linear(hidden_neurons, output_neurons, bias=False)  # Out fc
 
         # LIF cells
@@ -1428,20 +1427,10 @@ class ConvSNN11_no_dropout(nn.Module):
         seq_length = x.shape[0]
         batch_size = x.shape[1]
 
-        # # Dropout
-        # drop = nn.Dropout(p=0.5, inplace=True)
-        # mask_f1 = Variable(torch.ones(batch_size, self.hidden_neurons).cuda(), requires_grad=False)
-        # mask_f1 = drop(mask_f1)
-
         # specify the initial states
         sconv1 = sconv2 = sconv3 = sfc1 = so = None
-        # voltages = torch.zeros(
-        #     seq_length, batch_size, self.output_neurons, device=x.device, dtype=x.dtype
-        # )
         if flag is None:
             for ts in range(seq_length):
-                # print(f'Encoder: {(x[ts, :].count_nonzero() / x[ts, :].nelement()) * 100:.3f}%')
-
                 # First convolution
                 z = self.conv1(x[ts, :])
                 z, sconv1 = self.lif_conv1(z, sconv1)
@@ -1451,13 +1440,11 @@ class ConvSNN11_no_dropout(nn.Module):
                 z = self.conv2(z)
                 z, sconv2 = self.lif_conv2(z, sconv2)
                 z = self.avgpool(z)
-                # print(f'After conv1: {(z.count_nonzero() / z.nelement()) * 100:.3f}%')
 
                 # Third convolution
                 z = self.conv3(z)
                 z, sconv3 = self.lif_conv3(z, sconv3)
                 z = self.avgpool(z)
-                # print(f'After conv2: {(z.count_nonzero() / z.nelement()) * 100:.3f}%')
 
                 # Fully connected part
                 z = z.flatten(start_dim=1)
@@ -1465,7 +1452,6 @@ class ConvSNN11_no_dropout(nn.Module):
                 # First FC
                 z = self.fc1(z)
                 z, sfc1 = self.lif_fc1(z, sfc1)
-                # z = torch.mul(z, mask_f1)
 
                 # Fc out
                 z = self.fc_out(z)
@@ -1490,13 +1476,11 @@ class ConvSNN11_no_dropout(nn.Module):
                 z = self.conv2(z)
                 z, sconv2 = self.lif_conv2(z, sconv2)
                 z = self.avgpool(z)
-                # print(f'After conv1: {(z.count_nonzero() / z.nelement()) * 100:.3f}%')
 
                 # Third convolution
                 z = self.conv3(z)
                 z, sconv3 = self.lif_conv3(z, sconv3)
                 z = self.avgpool(z)
-                # print(f'After conv2: {(z.count_nonzero() / z.nelement()) * 100:.3f}%')
 
                 # Fully connected part
                 z = z.flatten(start_dim=1)
@@ -1504,7 +1488,6 @@ class ConvSNN11_no_dropout(nn.Module):
                 # First FC
                 z = self.fc1(z)
                 z, sfc1 = self.lif_fc1(z, sfc1)
-                # z = torch.mul(z, mask_f1)
                 hidden_spks[ts, :, :] = z
 
                 # Fc out
