31b1cbdcbb281ae3d8be252ab016819c7b22d721
110414 <aitor.martinez@tecnalia.com>
2023-02-27 12:47:22 +0100

    new hidden layers for bw

--- program_bw_benchmark.sh
+++ program_bw_benchmark.sh
@@ -1,5 +1,5 @@
 #!/usr/bin bash
 
-CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --pretrained --f-max 100 --n-time-steps 50 --cluster-mode predictions
-CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --pretrained --f-max 100 --n-time-steps 50 --cluster-mode correct-predictions
-CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 1 --pretrained --f-max 100 --n-time-steps 50 --cluster-mode labels
+CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 2 --pretrained --f-max 100 --n-time-steps 50 --cluster-mode predictions
+CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 2 --pretrained --f-max 100 --n-time-steps 50 --cluster-mode correct-predictions
+CUDA_VISIBLE_DEVICES=1 python main.py --conf bw-benchmark --n-hidden-layers 2 --pretrained --f-max 100 --n-time-steps 50 --cluster-mode labels
