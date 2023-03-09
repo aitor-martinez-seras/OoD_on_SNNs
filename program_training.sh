440dce355147b440ea5cdffa1df6c1492af13a26
110414 <aitor.martinez@tecnalia.com>
2023-03-09 10:14:53 +0100

    update sh scripts

--- program_training.sh
+++ program_training.sh
@@ -1,4 +1,3 @@
 #!/usr/bin bash
 
-CUDA_VISIBLE_DEVICES=1 python train.py --conf datasets --dataset CIFAR10 --model ConvNet --n-hidden-layers 10 --f-max 200 -b 128 --lr 0.002 --epochs 75 --weight-decay 0.00001 --opt adam --n-time-steps 64 --save-every 10  --lr-decay-milestones 10 20 25 30 --lr-decay-rate 0.8 --penultimate-layer-neurons 300
-CUDA_VISIBLE_DEVICES=1 python train.py --conf datasets --dataset CIFAR10 --model ConvNet --n-hidden-layers 10 --f-max 200 -b 128 --lr 0.002 --epochs 75 --weight-decay 0.00001 --opt adam --n-time-steps 64 --save-every 10  --lr-decay-milestones 10 20 25 30 --lr-decay-rate 0.8 --penultimate-layer-neurons 100
+CUDA_VISIBLE_DEVICES=0 python train.py --conf datasets --dataset CIFAR10 --model ConvNet --arch-selector 10 --f-max 200 -b 128 --lr 0.002 --epochs 75 --weight-decay 0.00001 --opt adamw --n-time-steps 64 --save-every 10  --lr-decay-milestones 10 20 25 30 --lr-decay-rate 0.8 --penultimate-layer-neurons 100
